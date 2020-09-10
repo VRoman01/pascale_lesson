d816a6f41707a730d2d48681f2bdd22cf0c8bb8f
vroman01 <venchak.201130@gmail.com>
2020-09-10 13:33:03 +0300

    created task_3

---
+++ task_3/task_3.pas
@@ -0,0 +1,13 @@
+procedure PrintDigitsOfNumber(n: integer);
+begin
+	if n>0 then
+	begin
+		PrintDigitsOfNumber(n div 10);
+		write(n mod 10, ' ')
+	end
+end;
+
+begin
+	PrintDigitsOfNumber(7583);
+	writeln()
+end.
